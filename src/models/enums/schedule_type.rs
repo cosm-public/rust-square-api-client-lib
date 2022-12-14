f0ad1741c3c875e4769ba645968a2c117f653749
nishantjinsi <nishantjinsi@gmail.com>
2022-12-14 14:12:56 -0800

    Add details for shipment type Delivery

---
+++ src/models/enums/schedule_type.rs
@@ -0,0 +1,14 @@
+//! Model for Schedule type enum
+
+use serde::{Deserialize, Serialize};
+
+/// Indicates the fulfillment delivery schedule type. If SCHEDULED, then deliver_at is required.
+/// If ASAP, then prep_time_duration is required. The default is SCHEDULED.
+#[derive(Clone, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "SCREAMING_SNAKE_CASE")]
+pub enum ScheduleType {
+    /// Indicates the fulfillment to deliver at a scheduled deliver time.
+    Scheduled,
+    /// Indicates that the fulfillment to deliver as soon as possible and should be prepared immediately.
+    Asap,
+}
